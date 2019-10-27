508008d7df90895568193e837f27c3069d99dcd1
Zilo676 <yolp@yandex.ru>
2019-10-27 17:56:36 +0300

    Добавлены mock

---
+++ src/mocks/offers.js
@@ -0,0 +1,32 @@
+const offers = [
+  {
+    id: `799663af-e554-49c5-b73e-d61b36b503f0`,
+    type: `Apartament`,
+    price: 120,
+    description: `Beautiful & luxurious apartment at great location`,
+    raiting: 93
+  },
+  {
+    id: `4a6cf5ff-150e-40f2-baf8-820461958a61`,
+    type: `Private room`,
+    price: 80,
+    description: `Wood and stone place`,
+    raiting: 80
+  },
+  {
+    id: `0dcee833-d3f2-4f12-9559-a2775ca7f733`,
+    type: `Apartament`,
+    price: 132,
+    description: `Canal View Prinsengracht`,
+    raiting: 80
+  },
+  {
+    id: `f5035346-5f36-480f-9401-b8f5dc8186c6`,
+    type: `Apartament`,
+    price: 180,
+    description: `Nice, cozy, warm big bed apartment`,
+    raiting: 100
+  }
+];
+
+export {offers};
