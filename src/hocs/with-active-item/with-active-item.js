183c98d0ac62bae8610258ded7d2fe9a741801c6
Zilo676 <yolp@yandex.ru>
2019-12-02 18:29:46 +0300

    Добавляет with-active-item hoc

---
+++ src/hocs/with-active-item/with-active-item.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const withActiveItem = (Component) => {
+  class WithActiveItem extends React.PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        item: null,
+      }
+
+      this._handleActiveItem = this._handleActiveItem.bind(this);
+    }
+
+    _handleActiveItem(item) {
+      this.setState({
+        item
+      });
+    }
+
+    render(){
+      return <Component
+      {...this.props}
+      onActiveItem={this._handleActiveItem}
+      />
+    }
+  }
+
+  WithActiveItem.propTypes = {};
+
+  return WithActiveItem;
+};
+
+export default withActiveItem;
