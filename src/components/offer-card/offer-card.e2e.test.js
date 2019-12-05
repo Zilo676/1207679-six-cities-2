eb5538be693937fa0c04a72933c5156d77e5996c
Zilo676 <yolp@yandex.ru>
2019-12-05 16:56:32 +0300

    Обновляет тесты

--- src/components/offer-card/offer-card.e2e.test.js
+++ src/components/offer-card/offer-card.e2e.test.js
@@ -12,16 +12,41 @@ it(`Card return value and invokes callback`, () => {
   const clickHandler = jest.fn();
   const event = {preventDefault: () => {}};
   const offer = {
-    type: `Apartament`,
+    id: 1,
+    city: {
+      name: `Amsterdam`,
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10
+      }
+    },
+    [`preview_image`]: `img/1.png`,
+    images: [`img/1.png`, `img/2.png`],
+    title: `Beautiful & luxurious studio at great location`,
+    [`is_favorite`]: false,
+    [`is_premium`]: false,
+    rating: 4.8,
+    type: `apartment`,
+    bedrooms: 3,
+    [`max_adults`]: 4,
     price: 120,
-    description: `Beautiful & luxurious apartment at great location`,
-    raiting: 93
+    goods: [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    host: {
+      id: 3,
+      [`is_pro`]: true,
+      name: `Angelina`,
+      [`avatar_url`]: `img/1.png`
+    },
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    }
   };
   const offerCard = shallow(<OfferCard
-    type={offer.type}
-    price={offer.price}
-    description={offer.description}
-    raiting={offer.raiting}
+    offer={offer}
     onOfferCardHover={clickHandler} />);
 
   const card = offerCard.find(`.place-card`);
