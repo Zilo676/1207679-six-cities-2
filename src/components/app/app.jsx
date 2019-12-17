753ee303b29b2e5b3ee9553e431fbec06748c897
Zilo676 <yolp@yandex.ru>
2019-12-18 00:14:57 +0300

    Добавляет hoc авторизации

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,16 +7,19 @@ import OfferDetails from '../offer-details/offer-details.jsx';
 import Favorites from '../favorites/favorites.jsx';
 
 import withSignIn from '../../hocs/with-sign-in/with-sign-in';
+import withAuth from '../../hocs/with-auth/with-auth';
 
 const App = () => {
 
   const SignInWrapped = withSignIn(SignIn);
+  const FavoritesWrapped = withAuth(Favorites);
+  const OfferDetailsWrapped = withAuth(OfferDetails);
 
   return (
     <Switch>
       <Route exact path='/login' component={SignInWrapped} />
-      <Route exact path='/offer/:id' render={({match}) => <OfferDetails id={+match.params.id} />} />
-      <Route exact path='/favorites' component={Favorites} />
+      <Route exact path='/offer/:id' render={({match}) => <OfferDetailsWrapped id={+match.params.id} />} />
+      <Route exact path='/favorites' component={FavoritesWrapped} />
       <Route exact path='/' component={MainPage} />
     </Switch>
   );
---
+++ src/hocs/with-auth/with-auth.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {getAuthorizationStatus} from '../../reducer/user/selectors';
+import {Redirect} from 'react-router-dom';
+
+const withAuth = (Component) => {
+  class WithAuth extends React.PureComponent {
+    constructor(props) {
+      super(props);
+    }
+
+    render() {
+      if (this.props.isAuthorizationRequired) {
+        return <Redirect to={"/login"} />
+      } else {
+        return <Component
+          {...this.props}
+        />;
+      }
+    }
+  }
+
+  WithAuth.propTypes = {
+    isAuthorizationRequired: PropTypes.bool.isRequired,
+  };
+
+  const mapStateTpProps = (state, ownProps) => Object.assign({}, ownProps, {
+    isAuthorizationRequired: getAuthorizationStatus(state),
+  });
+
+  return connect(mapStateTpProps)(WithAuth);
+}
+
+export default withAuth;
+
