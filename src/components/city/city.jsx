c34d1274211d3d4cb4b7cdc34dc8ee6acd0299b2
Zilo676 <yolp@yandex.ru>
2019-12-18 00:44:07 +0300

    Выделение активного city

--- src/components/city/city.jsx
+++ src/components/city/city.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
+import {getCurrentCity} from '../../reducer/city/selectors';
+
 import {ActionCreator} from '../../reducer/city/city';
 
 const City = (props) => {
-  const {city, onClick, onActiveItem} = props;
-
+  const {city, onClick, onActiveItem, currentCity} = props;
+  const activeCssClass = currentCity === city ? `tabs__item--active` : ``;
   return (<li className="locations__item" onClick={() => {
     onActiveItem(city);
     onClick(city);
   }}>
-    <span className="locations__item-link tabs__item">{city}</span>
+    <span className={`locations__item-link tabs__item ${activeCssClass}`}>{city}</span>
   </li>);
 };
 
@@ -19,6 +21,7 @@ City.propTypes = {
   city: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   onActiveItem: PropTypes.func.isRequired,
+  currentCity: PropTypes.string.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
@@ -27,6 +30,10 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
+const mapStateTpProps = (state, ownProps) => Object.assign({}, ownProps, {
+  currentCity: getCurrentCity(state),
+});
+
 export {City};
 
-export default connect(null, mapDispatchToProps)(City);
+export default connect(mapStateTpProps, mapDispatchToProps)(City);
